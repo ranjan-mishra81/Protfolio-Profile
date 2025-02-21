c079c3d83c36af97fc98f64ca92e333bb1822434
Ranjan Kumar Mishra <147745839+ranjan-mishra81@users.noreply.github.com>
2025-02-21 21:42:22 +0530

    upload file

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Full Stack Developer"  ,  "YouTuber" , "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
